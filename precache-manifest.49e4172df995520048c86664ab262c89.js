4a4efe35e1c079c3f84dc9ae7ac2b088758055c1
CondoriAvilaJoelAntonio <joel.antonio.condori.avila@gmail.com>
2020-07-24 09:06:32 -0500

    Updates

---
+++ precache-manifest.49e4172df995520048c86664ab262c89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0075f269005c601812d14b38b31781",
+    "url": "/practicasinsti/index.html"
+  },
+  {
+    "revision": "cb1f11e42c37c61db8fc",
+    "url": "/practicasinsti/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3fa13acb14891b60621c",
+    "url": "/practicasinsti/static/js/2.f9363ea6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/practicasinsti/static/js/2.f9363ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1f11e42c37c61db8fc",
+    "url": "/practicasinsti/static/js/main.d5a5b14f.chunk.js"
+  },
+  {
+    "revision": "5bba04833d9cc196f2b0",
+    "url": "/practicasinsti/static/js/runtime-main.1d532e4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/practicasinsti/static/media/logo.5d5d9eef.svg"
+  }
+]);
